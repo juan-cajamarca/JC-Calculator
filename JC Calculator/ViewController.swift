71a10f189b7602d66005e99c95260940feaccaf3
Juan Cajamarca <juancajamarca@outlook.es>
2020-04-12 14:17:41 -0500

    Initial Commit

---
+++ JC Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JC Calculator
+//
+//  Created by Juan David Cajamarca Acuña on 12/04/20.
+//  Copyright © 2020 Juan David Cajamarca Acuña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
